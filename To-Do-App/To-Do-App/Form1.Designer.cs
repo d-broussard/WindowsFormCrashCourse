3e0e198d991630606314ff23e606d3caeaaf4b1e
Devin Broussard <devin.ray.broussard@gmail.com>
2021-06-11 07:57:14 -0500

    update

---
+++ To-Do-App/To-Do-App/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace To_Do_App
+{
+    partial class ToDoApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHeader = new System.Windows.Forms.Label();
+            this.txtAdd = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.chklstTodos = new System.Windows.Forms.CheckedListBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblHeader
+            // 
+            this.lblHeader.AutoSize = true;
+            this.lblHeader.Location = new System.Drawing.Point(38, 23);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.Size = new System.Drawing.Size(85, 13);
+            this.lblHeader.TabIndex = 0;
+            this.lblHeader.Text = "Add New To-do:";
+            this.lblHeader.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtAdd
+            // 
+            this.txtAdd.Location = new System.Drawing.Point(41, 52);
+            this.txtAdd.Name = "txtAdd";
+            this.txtAdd.Size = new System.Drawing.Size(163, 20);
+            this.txtAdd.TabIndex = 1;
+            this.txtAdd.TextChanged += new System.EventHandler(this.txtAdd_TextChanged);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(210, 52);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(40, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chklstTodos
+            // 
+            this.chklstTodos.FormattingEnabled = true;
+            this.chklstTodos.Location = new System.Drawing.Point(41, 98);
+            this.chklstTodos.Name = "chklstTodos";
+            this.chklstTodos.Size = new System.Drawing.Size(209, 214);
+            this.chklstTodos.TabIndex = 3;
+            this.chklstTodos.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(41, 318);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(209, 23);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // ToDoApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(285, 368);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.chklstTodos);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtAdd);
+            this.Controls.Add(this.lblHeader);
+            this.Name = "ToDoApp";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.ToDoApp_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHeader;
+        private System.Windows.Forms.TextBox txtAdd;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.CheckedListBox chklstTodos;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ To-Do-App/To-Do-App/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace To_Do_App
+{
+    public partial class ToDoApp : Form
+    {
+        
+        public ToDoApp()
+        {
+            InitializeComponent();
+            this.Text = "ToDo App";
+        }
+
+        private void ToDoApp_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        // Works as an Add button for new todos
+        private void button1_Click(object sender, EventArgs e)
+        {
+            chklstTodos.Items.Add(txtAdd.Text);
+            txtAdd.Clear();
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtAdd_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            chklstTodos.Items.Clear();
+        }
+
+        private void btnClean_Click(object sender, EventArgs e)
+        {
+            chklstTodos.ClearSelected();
+        }
+    }
+}
